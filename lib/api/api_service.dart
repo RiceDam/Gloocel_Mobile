ac08f9383c6b138ff544efdf8f0d02f6848d335f
herbertma <hma49@sfu.ca>
2021-05-15 13:52:52 -0700

    Added progressbar on login + error showing + refactor code

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -7,11 +7,19 @@ class APIService {
   Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
     Uri url = Uri.http("10.0.2.2:8000", "/api/account/login");
 
-    var response = await http.post(url, body: requestModel.toJson());
+    try {
+      var response = await http.post(url, body: requestModel.toJson());
 
-    print(response.statusCode);
-    if (response.statusCode == 200 || response.statusCode == 400) {
-      return LoginResponseModel.fromJson(json.decode(response.body));
+      print(response.statusCode);
+      if (response.statusCode == 200 || response.statusCode == 400) {
+        return LoginResponseModel.fromJson(json.decode(response.body));
+      }
+    } catch (SocketException) {
+      // The design of the backend Django authentication allows for multiple errors
+      // So we should return an array of error messages, rather than just a single String
+      return LoginResponseModel.fromJson({
+        'non_field_errors': ['Unable to reach Gloocel Hub Servers']
+      });
     }
 
     throw Exception("Failed to load data");
